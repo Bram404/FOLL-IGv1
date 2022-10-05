9757a68263e1bb5348a30711ee3eea1774e9687f
04U-XD <113485604+Bram404@users.noreply.github.com>
2022-10-05 09:01:07 +0700

    Update set.php

--- set.php
+++ set.php
@@ -9,12 +9,12 @@
 
 # Masukan username akun ori/asli
 # Enter the original account username
-$youser="bramantyoooo_";
+$youser="iam.cici___";
 
 # Username akun tumbal
 # Enter the username of the account that was sacrificed
-$t_user="sanzz27";
+$t_user="willy__23";
 
 # Passwoard/sandi akun tumbal
 # enter the password of the account that was sacrificed
-$t_pass="sanzz23@";
+$t_pass="akas123";
