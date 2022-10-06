d3bf6abcbf034a1cee0d7ffadbd99b8dc80b167f
04U-XD <113485604+Bram404@users.noreply.github.com>
2022-10-07 06:02:10 +0700

    Update set.php

--- set.php
+++ set.php
@@ -13,8 +13,8 @@ $youser="riskaanisa2211";
 
 # Username akun tumbal
 # Enter the username of the account that was sacrificed
-$t_user="rzxvyop";
+$t_user="bramstore.id";
 
 # Passwoard/sandi akun tumbal
 # enter the password of the account that was sacrificed
-$t_pass="brama02";
+$t_pass="tiyut211";
